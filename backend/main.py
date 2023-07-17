95dd830c77364c5c8d87aa0ff28f37abd2952157
shreyanzjain <shreyanz32@gmail.com>
2023-07-17 22:05:32 +0530

    update: main.py

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,16 @@
 from fastapi import FastAPI, HTTPException, Request
+from src.database import SessionLocal, engine
+from sqlalchemy.orm import Session
+from src import models, schemas
+
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 app = FastAPI()
 
