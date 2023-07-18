18a5910be45827388956ccde2ac25d1b137363d3
shreyanzjain <shreyanz32@gmail.com>
2023-07-18 20:30:27 +0530

    add descriptive comments to database.py

--- backend/src/database.py
+++ backend/src/database.py
@@ -2,7 +2,11 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 
-# From env.py 
+# CREATE YOUR 'env.py' FILE IN 'BACKEND' AND DEFINE THESE VARIABLES AS PER YOUR POSTGRES CONFIG
+# PGUSR = Postgres user to log in
+# PGPASSWD = Postgres password for the aforesaid user
+# DBNAME =  Name of the db you want to access
+
 from env import PGUSR, PGPASSWD, DBNAME
 
 SQL_DATABASE_URL = f"postgresql://{PGUSR}:{PGPASSWD}@localhost/{DBNAME}"
