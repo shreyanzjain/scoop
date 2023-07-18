7e1731fcf53cddb556917b587d82ad557559f74d
shreyanzjain <shreyanz32@gmail.com>
2023-07-18 12:46:12 +0530

    update methods and main

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException, Request, Depends
+from typing_extensions import Annotated
 from src.database import SessionLocal, engine
 from sqlalchemy.orm import Session
-from src import models, schemas
+from src import models, schemas, methods
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -17,3 +18,11 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"Hello": "World"}
+
+@app.post("/user/create", response_model=schemas.User)
+def add_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    return methods.create_user(user, db)
+
+@app.post("/user/sessions")
+def auth_user(user: schemas.UserLogin, db: Session = Depends(get_db)):
+    return methods.authenticate_user(user, db)
---
+++ backend/src/methods.py
@@ -0,0 +1,29 @@
+from sqlalchemy.orm import Session
+from src import models, schemas
+import bcrypt
+from fastapi import HTTPException
+
+def create_user(user: schemas.UserCreate, db: Session):
+    hashed_password = bcrypt.hashpw(bytes(user.password, encoding="utf-8"), 
+                                    bcrypt.gensalt()).decode("utf-8")
+    new_user = models.User(username=user.username, password=hashed_password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+def authenticate_user(user: schemas.UserLogin, db: Session):
+    tmp_user = db.query(models.User).filter(models.User.username == user.username).first()
+    if tmp_user:
+        hashed_password = tmp_user.password
+        print(len(hashed_password))
+        is_valid = bcrypt.checkpw(bytes(user.password, "utf-8"), 
+                                  bytes(hashed_password, "utf-8"))
+        if is_valid:
+            # placeholder
+            return {"Valid": "Password"}
+        else:
+            # placeholder
+            return{"Invalid": "Password"}
+    else:
+        raise HTTPException(400, 'User not found.')
