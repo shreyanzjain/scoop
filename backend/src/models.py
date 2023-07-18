809f95ad3b0f8c3bb002d92dc6e6d7aff4a11f40
shreyanzjain <shreyanz32@gmail.com>
2023-07-18 12:46:29 +0530

    create the userscores model and schemas

--- backend/src/models.py
+++ backend/src/models.py
@@ -1,12 +1,22 @@
 from src.database import Base
-from sqlalchemy import Column, String, Date, Integer
+from sqlalchemy import Column, String, Date, Integer, Float
 from datetime import datetime
 
 class User(Base):
 
     __tablename__ = "users"
-    
+
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(18))
+    username = Column(String(18), unique=True)
     password = Column(String(64))
+    resume_path = Column(String)
     date_registered = Column(Date, default=datetime.utcnow)
+
+class UserScores(Base):
+
+    __tablename__ = "user_scores"
+
+    id = Column(Integer, primary_key=True, index=True)
+    job_title = Column(String)
+    company = Column(String)
+    score = Column(Float)
--- backend/src/schemas.py
+++ backend/src/schemas.py
@@ -9,10 +9,32 @@ class UserCreate(UserBase):
 
     password: str
 
+class UserLogin(UserCreate):
+
+    pass
+
+class UserResumeUpload(UserBase):
+
+    resume_path: str
+
 class User(UserBase):
 
     id: int
     date_registered: datetime
+
+    class Config:
+        from_attributes = True
+
+class UserScoresBase(BaseModel):
+    pass
+
+class UserScoresCreate(UserScoresBase):
+    job_title: str
+    company: str
+    score: float
+
+class UserScores(UserScoresCreate):
+    id: int
 
     class Config:
         from_attributes = True
