40650d68b1851771d21c19f0724a741927be5562
shreyanzjain <shreyanz32@gmail.com>
2023-07-18 13:37:20 +0530

    add: get and post methods for scores

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException, Request, Depends
+from typing import List
 from typing_extensions import Annotated
 from src.database import SessionLocal, engine
 from sqlalchemy.orm import Session
@@ -15,6 +16,9 @@ def get_db():
 
 app = FastAPI()
 
+# placeholder logged in user
+curr_user_id: int = 1
+
 @app.get("/")
 def home():
     return {"Hello": "World"}
@@ -26,3 +30,11 @@ def add_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 @app.post("/user/sessions")
 def auth_user(user: schemas.UserLogin, db: Session = Depends(get_db)):
     return methods.authenticate_user(user, db)
+
+@app.post("/user/{user_id}/score", response_model=schemas.UserScores)
+def add_score(user_id: int, create_score: schemas.UserScoresCreate, db: Session = Depends(get_db)):
+    return methods.add_score(user_id, create_score, db)
+
+@app.get("/user/score", response_model=List[schemas.UserScores])
+def get_user_posts(db: Session = Depends(get_db)):
+    return db.query(models.UserScores).filter(models.UserScores.user_id == curr_user_id).all()
