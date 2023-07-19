eac6c39633a512402c0034f239b430c63af3cbff
shreyanzjain <shreyanz32@gmail.com>
2023-07-19 20:38:13 +0530

    add: validation checks to the api's

--- backend/main.py
+++ backend/main.py
@@ -25,15 +25,23 @@ def home():
 
 @app.post("/user/create", response_model=schemas.User)
 def add_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    return methods.create_user(user, db)
+    check_user = db.query(models.User).filter(models.User.username == user.username).first()
+    if(check_user):
+        raise HTTPException(409, 'Username already taken!')
+    else:
+        return methods.create_user(user, db)
 
 @app.post("/user/sessions")
 def auth_user(user: schemas.UserLogin, db: Session = Depends(get_db)):
     return methods.authenticate_user(user, db)
 
-@app.post("/user/{user_id}/score", response_model=schemas.UserScores)
-def add_score(user_id: int, create_score: schemas.UserScoresCreate, db: Session = Depends(get_db)):
-    return methods.add_score(user_id, create_score, db)
+@app.post("/user/{username}/score", response_model=schemas.UserScores)
+def add_score(username: str, create_score: schemas.UserScoresCreate, db: Session = Depends(get_db)):
+    check_user = db.query(models.User).filter(models.User.username == username).first()
+    if(not check_user):
+        raise HTTPException(404, 'User Does Not Exist!')
+    else:
+        return methods.add_score(username, create_score, db)
 
 @app.get("/user/score", response_model=List[schemas.UserScores])
 def get_user_posts(db: Session = Depends(get_db)):
